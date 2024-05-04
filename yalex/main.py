c13b31b76748ecfe7328f7294e704b86027efb86
Estuardo07 <xavihernand7@hotmail.com>
2024-05-03 22:36:29 -0600

    updates

---
+++ yalex/main.py
@@ -0,0 +1,25 @@
+from yalexReader import yalexReader
+from directAFD import regex_to_afd
+from jinja2 import Template
+import pickle
+
+
+def main():
+    file = 'slr-4.yal'
+    rule_token, token_dic = yalexReader(file)
+    with open('template.j2', 'r') as f:
+        template = f.read()
+
+    template = Template(template)
+    rendered = template.render(tokens=token_dic)
+    with open('scanner.py', 'w') as f:
+        f.write(rendered)
+    afd = regex_to_afd(rule_token, token_dic)
+
+    print("afd generado")
+    with open('afd.pickle', 'wb') as f:
+        pickle.dump(afd, f)
+
+
+if __name__ == '__main__':
+    main()
